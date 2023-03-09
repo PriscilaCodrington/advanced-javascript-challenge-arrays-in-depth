812ddc2b8442e18fd95328276fa11a2e00225d65
bguzmanrio <bguzmanrio91@gmail.com>
2023-03-09 21:43:15 +0100

    Update fields on select change

--- web/index.js
+++ web/index.js
@@ -5,20 +5,20 @@ import { POKEMONS, EXPECTATION_ITEMS } from '../data/expectations.js';
 
 const grid = document.querySelector('#grid');
 
-const formatJson = output => JSON.stringify(output, null, 2)
+const formatJson = output => JSON.stringify(output, null, 2);
 
-const createOutput = (content, functionName) => {
-  const wrapper = document.createElement('div');
-  
+const createOption = (name, value) => `<option value="${value}">${name.join(', ')}</option>`;
+
+const createOutputContent = (content, functionName) => {
   const pre = document.createElement('pre');
   const code = document.createElement('code');
 
+  pre.appendChild(code);
+
   const splittedContent = content.split('\n');
 
-  wrapper.className = 'col adv__code';
-  wrapper.appendChild(pre);
-
   if (splittedContent.length > 4) {
+    const wrapper = document.createElement('span');
     const toggle = document.createElement('a');
     const collapsedPre = document.createElement('pre');
     const collapsedCode = document.createElement('code');
@@ -34,75 +34,40 @@ const createOutput = (content, functionName) => {
 
     collapsedPre.appendChild(collapsedCode);
 
+    wrapper.appendChild(pre);
     wrapper.appendChild(collapsedPre);
     wrapper.appendChild(toggle);
+
+    return wrapper;
   } else {
     code.innerText = content;
-  }
+  }  
 
-  pre.appendChild(code);
-
-  return wrapper;
-};
-
-const createOption = (name, value) => `<option value="${value}">${name.join(', ')}</option>`;
-
-const createInput = expectations => {
-  const wrapper = document.createElement('div');
-  wrapper.className = 'col';
-
-  if(expectations.length > 1) {
-    const select = document.createElement('select');
-    select.className = 'form-select';
-    select.innerHTML = expectations.map((expectation, i) => createOption(expectation.input, i));
-    wrapper.appendChild(select);
-  } else {
-    wrapper.innerHTML = 'N/A';
-  }
-  
-  return wrapper;
+  return pre;
 }
 
-const createRow = () => {
+const createRow = (content, functionName) => {
+  let selectedInput = content.expectations[0].input;
+  let expectedOutput = content.expectations[0].output;
   const row = document.createElement('div');
+  const name = document.createElement('div');
+  const input = document.createElement('div');
+  const output = document.createElement('div');
+  const result = document.createElement('div');
   row.className = 'row';
 
-  return row;
-};
-
-const createName = (description, functionName) => {
-  const name = document.createElement('div');
-  name.innerText = functionName;
-  name.title = description;
-  name.className = 'col';
-
-  return name;
-};
-
-const createResult = (handleClick) => {
-  const result = document.createElement('div');
-  const button = document.createElement('button');
-  button.type = 'button';
-  button.className = 'btn btn-primary';
-  button.innerText = 'Run!';
-  button.addEventListener('click', handleClick);
-
-  result.className = 'col';
-  result.appendChild(button);
-
-  return result;
-}
-
-Object.entries(EXPECTATION_ITEMS).forEach(([functionName, content]) => {
-  const { input, output } = content.expectations[0];
+  row.appendChild(name);
+  row.appendChild(input);
+  row.appendChild(output);
+  row.appendChild(result);
 
   const handleRunClick = () => {
-    const executionResult = utils[functionName](...[...input, POKEMONS]);
+    const executionResult = utils[functionName](...[...selectedInput, POKEMONS]);
 
     const button = result.querySelector('button');
     result.querySelector(`[data-output=${functionName}`)?.remove();
 
-    if (isEqual(executionResult,output)) {
+    if (isEqual(executionResult, expectedOutput)) {
       button.className = 'btn btn-success';
       button.innerText = 'Success ✔️';
     } else {
@@ -110,21 +75,89 @@ Object.entries(EXPECTATION_ITEMS).forEach(([functionName, content]) => {
       button.innerText = 'Not equal ⛌';
     }
 
-    const showResult = createOutput(formatJson(executionResult) || 'undefined', `result-${functionName}`);
+    const showResult = createOutputContent(formatJson(executionResult) || 'undefined', `result-${functionName}`);
     showResult.dataset.output = functionName;
     result.appendChild(showResult);
+  };
+
+  const createName = () => {
+    name.innerText = '';
+    name.innerText = functionName;
+    name.title = content.name;
+    name.className = 'col';
+  };
+
+  const createInput = (expectations, handleSelectInput) => {
+    name.innerText = '';
+    input.className = 'col';
+  
+    if(expectations.length > 1) {
+      const select = document.createElement('select');
+      select.className = 'form-select';
+      select.innerHTML = expectations.map((expectation, i) => createOption(expectation.input, i));
+      input.appendChild(select);
+      select.addEventListener('change', handleSelectInput);
+    } else {
+      input.innerHTML = 'N/A';
+    }
+  };
+
+  const createOutput = () => {  
+    output.innerText = '';
+    const content = formatJson(expectedOutput);
+    const pre = createOutputContent(content, functionName);
+
+    output.className = 'col adv__code';
+    output.appendChild(pre);
+  };
+
+  const createResult = () => {
+    result.innerText = '';
+
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.className = 'btn btn-primary';
+    button.innerText = 'Run!';
+    button.addEventListener('click', handleRunClick);
+
+    result.className = 'col';
+    result.appendChild(button);
   }
 
-  const row = createRow();
-  const inputContent = createInput(content.expectations);
-  const expected = createOutput(formatJson(output), functionName);
-  const name = createName(content.name, functionName);
-  const result = createResult(handleRunClick);
+  const updateSelectedInput = newInput => {
+    selectedInput = content.expectations[newInput].input;
+    expectedOutput = content.expectations[newInput].output;
+
+    createOutput();
+    createResult();
+  }
+
+  return {
+    htmlElement: row,
+    createName,
+    createInput,
+    createOutput,
+    createResult,
+    updateSelectedInput
+  };
+};
+
+
+Object.entries(EXPECTATION_ITEMS).forEach(([functionName, content]) => {
+  const row = createRow(content, functionName);
+
+  const handleSelectInput = (e) => {
+    const selected = parseInt(e.target.value, 10);
+    row.updateSelectedInput(selected);
+  };
+
   
-  row.appendChild(name);
-  row.appendChild(inputContent);
-  row.appendChild(expected);
-  row.appendChild(result);
-  grid.appendChild(row);
+
+  row.createInput(content.expectations, handleSelectInput);
+  row.createOutput();
+  row.createName(content.name);
+  row.createResult();
+  
+  grid.appendChild(row.htmlElement);
 })
 
